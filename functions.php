15255e8a3a54c7f0a5fac91ce08acf35168e4a30
Marin @Home <marin@marincarroll.com>
2022-06-04 12:42:48 -0400

    Feat: svg upload support

--- functions.php
+++ functions.php
@@ -3,6 +3,9 @@
  * @package groundhog
  */
 
+/**
+ * Enqueue theme stylesheet
+ */
 function groundhog_enqueue_assets() {	
     $theme_path = '/assets/css/style.css';
 	wp_enqueue_style(
@@ -13,3 +16,22 @@ function groundhog_enqueue_assets() {
 	);
 }
 add_action('enqueue_block_assets', 'groundhog_enqueue_assets');
+
+
+/**
+ * Allow for SVG uploads in Media Library
+ **/
+function groundhog_disable_real_mime_check( $data, $file, $filename, $mimes ) {
+    $filetype = wp_check_filetype( $filename, $mimes );
+    $ext    = $filetype['ext'];
+    $type   = $filetype['type'];
+    $proper = $data['proper_filename'];
+    return compact( 'ext', 'type', 'proper_filename' );
+ }
+ add_filter( 'wp_check_filetype_and_ext', 'groundhog_disable_real_mime_check', 10, 4 );
+ 
+ function groundhog_mime_types($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+ }
+ add_filter('upload_mimes', 'groundhog_mime_types');
